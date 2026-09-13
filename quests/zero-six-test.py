346f182fe03bc5a91fc46484694dbb9533671723
Pannakotta <panissimo13@gmail.com>
2026-09-13 19:37:54 +0300

    updating task 0.6

--- quests/zero-six-test.py
+++ quests/zero-six-test.py
@@ -1,2 +1,5 @@
-health_player = int(input("Сколько у вас здоровья?"))
-print("Ваше здоровье: ", health_player) 
+health_player = int(input("Сколько у вас здоровья? "))
+if (health_player >= 100):
+    print("Ваше здоровье: ", health_player) 
+else:
+    print("Ваше здоровье слишком большое!")
