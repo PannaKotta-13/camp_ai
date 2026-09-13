14663722228ea5b0cc2d42b2bbc50bc57921c188
Pannakotta <panissimo13@gmail.com>
2026-09-13 19:49:44 +0300

    Revert "merge conflict fix in task 0.6"
    This reverts commit 49359f58ad1e000b278f9a8e838d24390e9fd153.

--- quests/zero-six-test.py
+++ quests/zero-six-test.py
@@ -3,5 +3,3 @@ if (health_player <= 100):
     print("Ваше здоровье: ", health_player) 
 else:
     print("Ваше здоровье слишком большое!")
-health_player = int(input("Сколько у вас здоровья?"))
-print("Ваше здоровье: ", health_player) 
