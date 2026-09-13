fcfdcdcf2396f698e5ff37319af309472ef0bef2
Pannakotta <panissimo13@gmail.com>
2026-09-13 19:39:22 +0300

    bug fix in task 0.6. 0.6 is correctly done!

--- quests/zero-six-test.py
+++ quests/zero-six-test.py
@@ -1,5 +1,5 @@
 health_player = int(input("Сколько у вас здоровья? "))
-if (health_player >= 100):
+if (health_player <= 100):
     print("Ваше здоровье: ", health_player) 
 else:
     print("Ваше здоровье слишком большое!")
